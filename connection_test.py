92f6dbdd24bccb1d727d60386a3d59b986b10338
bragatheesh <bragatheesh.s@gmail.com>
2016-10-19 21:21:03 -0700

    Code has been tested
    Code has been tested and some small changes were made to fix bugs
    regarding corner case when ecu does not respon in an expected fashion

--- connection_test.py
+++ connection_test.py
@@ -43,14 +43,19 @@ try:
 
     data = "00 00"
     
-    while data == "00 00"
+    while data == "00 00":
     #TODO: see what happens when RPM is polled with 1. Car OFF; 2. Key Pos 1; 3. Key Pos 2;
         tn.write("010C\r\n") #sends a request for RPM 
-        data = tn.read_until("STOPPED", 2) #reads for up to 2 seconds or until the word "STOPPED" appears //need to find better way
+        data = tn.read_until("UNABLE TO CONNECT", 2) #reads for up to 2 seconds or until the word "STOPPED" appears //need to find better way
         data = data.split("0C ", 1) #splits the resulting response by 0C, the string after 0C contains RPM data
-        data = data[1] #sets data as the RPM data string
-        f.write(data) #writes RPM to file followed by a newline
-        f.write ("\n")
+        if len(data) == 2:
+            print "data size is 2"
+            data = data[1] #sets data as the RPM data string
+            print "data after split " + data
+            f.write(data) #writes RPM to file followed by a newline
+            f.write ("\n")
+        else:
+            data = "00 00"
         time.sleep(1) #to prevent overflowing OBD buffer
 
     ENG_ON = 1
