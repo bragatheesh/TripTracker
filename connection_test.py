e5afba71da8536764bcc7848495a9486d8b44fca
bragatheesh <bragatheesh.s@gmail.com>
2016-10-23 13:27:22 -0700

    Parsing fixes and new features with MAF
    Fixed some of the split delimters. Added new error checking to handle
    cars that don't have MAF.
    
    CURRENTLY NOT WORKING
    
    TODO: Fix detection of car turning on

--- connection_test.py
+++ connection_test.py
@@ -21,20 +21,25 @@ def wait_car_on(): #used to wait until car is on
     while rpm == "00 00":
 
         tn.write("010C\r\n") #sends a request for RPM 
-        rpm = tn.read_until("UNABLE TO CONNECT", 2) #reads for up to 2 seconds or until the word "UNABLE TO CONNECT" appears
+        rpm = tn.read_until("UNABLE TO CONNECT", 5) #reads for up to 5 seconds or until the word "UNABLE TO CONNECT" appears
 
-        rpm = rpm.split("0C ", 1) #splits the resulting response by 0C, the string after 0C contains RPM data
+        rpm = rpm.split(" 0C ", 1) #splits the resulting response by 0C, the string after 0C contains RPM data
 
         if len(rpm) == 2: 
             #print "data size is 2"
-            rpm = rpm[1] #sets data as the RPM data string    
+            rpm = rpm[1] #sets data as the RPM data string
 
+            if len(rpm) > 5: #this is to strip off all except for the first 5 characters
+                lenght = len(rpm)
+                length = length - 5
+                length = -1 * length
+                rpm = rpm[:length]
         else:
             rpm = "00 00" #added to cover case when ECU does not respond to RPM request and sends unexpected data 
 
         time.sleep(1) #to prevent overflowing OBD buffer
 
-    f.write("Car turned on at" + time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime()) + "\n\n\n")
+    f.write("Car turned on at " + time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime()) + "\n\n\n")
     print "CAR IS ON!"
     return 1
 
@@ -43,13 +48,19 @@ def check_car_off(): #used to check if car is off
     tn.write("010C\r\n") 
     rpm = tn.read_until("UNABLE TO CONNECT", 2) 
 
-    rpm = rpm.split("0C ", 1)
+    rpm = rpm.split(" 0C ", 1)
 
-    if len(rpm) != 2: #covers the case when ECE does not return anything because car is off
+    if len(rpm) != 2: #covers the case when ECU does not return anything because car is off
         print "CAR IS OFF"
         return 0
 
     rpm = rpm[1]
+
+    if len(rpm) > 5: #this is to strip off all except for the first 5 characters
+        lenght = len(rpm)
+        length = length - 5
+        length = -1 * length
+        rpm = rpm[:length]
 
     if rpm == "00 00":
         print "CAR IS OFF"
@@ -85,51 +96,86 @@ try:
     now = time.time() #stores current time into "now"
     done = now + 10 #stores 10 seconds from "now" into "done"
 
+    print "Waiting for car to turn on"
     ENG_ON = wait_car_on() #wait till car is on
 
+    print "Getting initial fuel level"
     if ENG_ON == 1: #grabbing initial fuel level
         tn.write("012F\r\n") #sends a request for fuel level 
         iFuel = tn.read_until("UNABLE TO CONNECT", 2)
 
-        iFuel = iFuel.split("2F ", 1) 
-        iFuel = iFuel[1]
+        iFuel = iFuel.split(" 2F ", 1) 
 
         if len(iFuel) == 2:
+            iFuel = iFuel[1]
+
+            if len(iFuel) > 2: #this is to strip off all except for the first 2 characters
+                lenght = len(iFuel)
+                length = length - 2
+                length = -1 * length
+                iFuel = iFuel[:length]
+
             INIT_FUEL_LEVEL = int(iFuel, 0)
             f.write("Initial Fuel Level: " + iFuel + "\n\n\n") #write initial fuel level as the first thing on the file
+            print "Initial Fuel Level: " + iFuel
 
         else:
             f.write("Initial Fuel Level: N/A\n\n\n") #if ECU does not support fuel level input or unexpected output was returned
-            
+            print "Failed to get initial fuel level"
+
+    print "Driving"        
     while ENG_ON == 1: #this is where we spin gathering speed data and checking to see if the trip has ended
 
         tn.write("010D\r\n") #sends a request for vehicle speed
-        speed = tn.read_until("UNABLE TO CONNECT", 2)
-
+        speed = tn.read_until("NO DATA", 2)
+        print "speed: " + speed
+        
         tn.write("0110\r\n") #requests Mass Air Flow rate which is used for calculating MPG
-        maf = tn.read_until("UNABLE TO CONNECT", 2)
+        maf = tn.read_until("NO DATA", 2)
+        print "maf: " + maf
 
-        speed = speed.split("0D ", 1)
-        speed = speed[1]
+        if maf == "NO DATA" and speed == "NO DATA":
+            print "No data recieved for both speed and maf, exiting"
+            break
+        
+        speed = speed.split(" 0D ", 1)
+        if len(speed) == 2:
+            speed = speed[1]
+            if len(speed) > 2: #this is to strip off all except for the first 2 characters
+                lenght = len(speed)
+                length = length - 2
+                length = -1 * length
+                speed = speed[:length]
+            print "speed after split: " + speed
+        else:
+            break
 
-        maf = maf.split("10 ", 1)
-        maf = maf[1]
+        maf = maf.split(" 10 ", 1)
+        if len(maf) == 2:
+            maf = maf[1]
+            print "maf after split: " + maf
+        else:
+            break
 
         f.write(time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime()) + " " + speed + " " + maf + "\n") #formula for calculating MPG is VSS * 7.718 / MAF
+        print time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime()) + " " + speed + " " + maf 
 
         ENG_ON = check_car_off() #check to see if car has been turned off
 
         time.sleep(5) #wait five seconds before polling speed and MAF again
 
-    #if we break out, it's because engine is off. Grab final fuel level and exit
+    #if we break out, it's because engine is off or something went wrong. Try to grab final fuel level and exit
 
     tn.write("012F\r\n") #sends a request for fuel level 
-    fFuel = tn.read_until("UNABLE TO CONNECT", 2)
+    fFuel = tn.read_until("UNABLE TO CONNECT", 10)
 
-    fFuel = fFuel.split("2F ", 1) 
-    fFuel = fFuel[1]
+    if fFuel == "UNABLE TO CONNECT":
+        raise Exception('no final fuel val')
+    
+    fFuel = fFuel.split(" 2F ", 1) 
 
     if len(fFuel) == 2:
+        fFuel = fFuel[1]
         FIN_FUEL_LEVEL = int(fFuel, 0)
         f.write("Final Fuel Level: " + fFuel + "\n\n\n")
 
@@ -148,3 +194,8 @@ except KeyboardInterrupt: #catches CTRL-C and exits gracefully
     print "Exiting"
     tn.close()
     f.close()
+
+except:
+    print "Unexpected Error, closing files and exiting"
+    f.close()
+    tn.close()
